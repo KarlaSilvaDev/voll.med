926008391403c1a862f1880741c8e0491fa779e0
Karlasilvaeng <karlasilvaeng@gmail.com>
2024-07-10 19:51:47 -0300

    feat: list patients

--- src/main/java/med/voll/api/controller/PatientController.java
+++ src/main/java/med/voll/api/controller/PatientController.java
@@ -4,10 +4,14 @@ import jakarta.transaction.Transactional;
 
 import jakarta.validation.Valid;
 import med.voll.api.dto.PatientDetailsDTO;
+import med.voll.api.dto.PatientListingDTO;
 import med.voll.api.dto.PatientRegistrationDTO;
 import med.voll.api.model.Patient;
 import med.voll.api.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -28,5 +32,11 @@ public class PatientController {
         var uri = uriBuilder.path("/patients/{id}").buildAndExpand(patient.getId()).toUri();
 
         return ResponseEntity.created(uri).body(new PatientDetailsDTO(patient));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<PatientListingDTO>> list(@PageableDefault(size=10, sort = {"name"}) Pageable pagination){
+        var page = repository.findAll(pagination).map(PatientListingDTO::new);
+        return ResponseEntity.ok(page);
     }
 }
---
+++ src/main/java/med/voll/api/dto/PatientListingDTO.java
@@ -0,0 +1,17 @@
+package med.voll.api.dto;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import med.voll.api.model.Address;
+import med.voll.api.model.Patient;
+
+public record PatientListingDTO(
+        Long id,
+        @JsonAlias("nome")
+        String name,
+        String email,
+        String cpf
+) {
+    public PatientListingDTO(Patient patient) {
+        this(patient.getId(), patient.getName(), patient.getEmail(), patient.getCpf());
+    }
+}
