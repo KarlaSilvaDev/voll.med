ce03bcb25b98abfdbf9a3614f5d860398b6ea677
Karlasilvaeng <karlasilvaeng@gmail.com>
2024-07-11 12:34:35 -0300

    feat: included user entity, repository and authentication service

--- src/main/java/med/voll/api/domain/user/AuthenticationService.java
+++ src/main/java/med/voll/api/domain/user/AuthenticationService.java
@@ -1,2 +1,18 @@
-package med.voll.api.domain.user;public class AuthenticationService {
+package med.voll.api.domain.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService implements UserDetailsService {
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByLogin(username);
+    }
 }
--- src/main/java/med/voll/api/domain/user/User.java
+++ src/main/java/med/voll/api/domain/user/User.java
@@ -1,2 +1,22 @@
-package med.voll.api.domain.user;public class User {
+package med.voll.api.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "User")
+@Table(name = "users")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String login;
+    private String password;
 }
